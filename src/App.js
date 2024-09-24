f766f56e77e24b52f43d8f9bad068d67bd84f0c1
pigstina <christinakwekwanjing@gmail.com>
2024-09-25 00:16:41 +0800

    test design

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Welcome to My Bookstore</h1>
-        <nav>
+        <h1>Bookstore</h1>
+        {/* <nav>
           <a href="#bestsellers">Bestsellers</a>
           <a href="#categories">Categories</a>
           <a href="#contact">Contact</a>
-        </nav>
+        </nav> */}
       </header>
+      <div className="banner">
+          <div className="banner-img">
+            <img src="/banner.jpg" alt="stack-of-books"/>
+        </div>
+      </div>
       <section id="bestsellers">
         <h2>Bestselling Books</h2>
         {/* Add a list or carousel of books here */}
       </section>
       <section id="categories">
-        <h2>Shop by Category</h2>
-        {/* Add book categories here */}
+        {/* <h2>Shop by Category</h2>
+        Add book categories here */}
       </section>
       <footer>
         <p>Contact us at bookstore@example.com</p>
